c07bce4ff4c8d60362303ddd644701dd35604e45
Navi <superokvv@gmail.com>
2020-07-06 00:36:11 -0500

    update log filepath, add timestamp

--- utils/Log.cpp
+++ utils/Log.cpp
@@ -12,8 +12,19 @@ void Log::setPath(const char * path, const char * mode) {
 		fclose(pFile);
 		pFile = nullptr;
 	}
+	
+	char drive[32] = {};
+	char dir[256] = {};
+	char filename[256] = {};
+	char ext[32] = {};
+	_splitpath(path, drive, dir, filename, ext);
 
-	pFile = fopen(path, mode);
+	char file[1024] = {};
+
+	time_t now = system_clock::to_time_t(system_clock::now());
+	std::tm *pNow = localtime(&now);
+	sprintf(file, "%s%s%s_%d_%d_%d_%d_%d_%d%s", drive, dir, filename, pNow->tm_year + 1900, pNow->tm_mon + 1, pNow->tm_mday, pNow->tm_hour, pNow->tm_min, pNow->tm_sec, ext);
+	pFile = fopen(file, mode);
 
 	if (pFile == nullptr) {
 		LOG_ERROR("Set log path - Fail! PATH=%s, MODE=%s\n", path, mode);
